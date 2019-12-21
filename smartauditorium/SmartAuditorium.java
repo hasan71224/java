9699f72ab8440ceb4bfd00f9bf656f8bd64a3018
Md. Mahmudul Hasan <59121939+hasan71224@users.noreply.github.com>
2019-12-22 01:29:35 +0600

    Add files via upload

---
+++ smartauditorium/SmartAuditorium.java
@@ -0,0 +1,12 @@
+package smartauditorium;
+
+
+public class SmartAuditorium {
+
+    public static void main(String[] args) {
+      home obj = new home();
+        obj.setVisible(true);  
+        
+    }
+    
+}
